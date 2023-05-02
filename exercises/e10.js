3a7341dda0e2f914b9ab5dbd0037fb7119cfdf4f
Eric Donnelly <98561888+EricDonnelly-Dev@users.noreply.github.com>
2023-05-02 14:50:01 -0400

    finished e7,8,9 , e10 passes tests, but notes that errors are appearing in the test file not sure if complete

--- exercises/e10.js
+++ exercises/e10.js
@@ -17,21 +17,26 @@ const promiseArr = [promise1, promise2, promise3, promise4];
  * when promiseArr was passed as the argument
  */
 
-export const result1 = val; // Your code here
+export const result1 = Promise.all(promiseArr)
+.catch((err) => console.log(err.mesage)); 
 
 /**
  * @task
  * Use a correct PROMISE shortcut that will log and return the resolved value of promise3;
  */
 
-export const result2 = val; // Your code here
+export const result2 = Promise.any(promiseArr)
+.then((val) => console.log(val)); 
 
 /**
  * @task
- * Use a correct PROMISE shortcut that will log and return an array of all promises statuses and values/reasons;
+ * Use a correct PROMISE shortcut that will log 
+ * and return an array of all promises statuses and values/reasons;
  */
 
-export const result3 = val; // Your code here
+export const result3 = Promise.allSettled(promiseArr)
+.then((val) => console.log(val))
+.catch((err) => console.log(err));
 
 /**
  * @task
@@ -42,7 +47,8 @@ export const result3 = val; // Your code here
  * Example: export const newPromiseArr = promiseArr.<method>()...
  */
 
-export const newPromiseArr = val; // Your code here
+export const newPromiseArr = promiseArr.filter((val) => val === promise4);
+ 
 
 // Do NOT refactor or update result 4, it's all set to work
 export const result4 = Promise.race(newPromiseArr)
